e121a9231598aff120a4afff1ba57c00669b585c
padilha <igor.padilha2004@gmail.com>
2023-05-31 00:44:32 -0300

    try to fix base url for vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const urlBase = globalThis.process.env.APP_BASE_URL || '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    base: urlBase
+  },
   plugins: [react()],
 })
